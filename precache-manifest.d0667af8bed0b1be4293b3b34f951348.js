022139c0cae0ce4bc5578a85e74aae317dd72df1
Bartek Krupa <krupa1.bk@gmail.com>
2019-08-22 21:02:57 +0200

    Updates

---
+++ precache-manifest.d0667af8bed0b1be4293b3b34f951348.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ed14207bfb981548df73f2674b3c4c",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "145d9af1c839b8efcd7b",
+    "url": "/react-quiz/static/css/main.4ea38504.chunk.css"
+  },
+  {
+    "revision": "d7edd717c7431821931e",
+    "url": "/react-quiz/static/js/2.cbee8a74.chunk.js"
+  },
+  {
+    "revision": "145d9af1c839b8efcd7b",
+    "url": "/react-quiz/static/js/main.263dd1f1.chunk.js"
+  },
+  {
+    "revision": "bf180eaa784d204482a2",
+    "url": "/react-quiz/static/js/runtime~main.595449da.js"
+  },
+  {
+    "revision": "a8fa366d0b79a294685c08fc0d465f1a",
+    "url": "/react-quiz/static/media/brain.a8fa366d.png"
+  }
+]);
